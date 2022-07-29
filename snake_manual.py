23c9ee2720fc2357f3d7ce315347474bed7eccd2
milind <milindchaudhary80@gmail.com>
2022-07-29 11:58:56 +0200

    namedtuple point created

--- snake_manual.py
+++ snake_manual.py
@@ -8,14 +8,23 @@ import random
 from enum import Enum
 from collections import namedtuple
 
+# initialize all imported pygame modules
 pygame.init()
+
 # setting up the font
 font = pygame.font.Font('arial.ttf', 25)
 
 # making class of keys for the Direction 
 # inheriting from class Enum
+# Enum is a class for creating enumerations which are a set of
+# symbolic names (members) bound to unique, constant values.
 class Direction(Enum):
     RIGHT = 1
     LEFT = 2
     UP =3
     DOWN = 4
+
+ 
+#  creating a namedtuple for the x and y cordinates
+# basically it names the cordinates as x= and y= 
+Point = namedtuple('Point','x,y')
