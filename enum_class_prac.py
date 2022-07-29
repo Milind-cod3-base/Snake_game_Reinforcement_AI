a7743cef20c2496eeb169c376c398d7358cd2134
milind <milindchaudhary80@gmail.com>
2022-07-29 10:52:59 +0200

    enum printing: type, repr methods used

--- enum_class_prac.py
+++ enum_class_prac.py
@@ -13,3 +13,13 @@ class Days(Enum):
 # print the enum member as a string
 print("The enum memeber as a string is: ", end="")
 print(Days.Mon)
+
+# print the enum member as a repr
+print("The enum member as a repr is:", end = "")
+print(repr(Days.Sun)) # This gives the entire detail about the member
+
+# print the name of the enum member
+print(Days.Tue.name)
+
+# Check the type of the enum member
+print(type(Days.Mon))  # gets the name of the class
