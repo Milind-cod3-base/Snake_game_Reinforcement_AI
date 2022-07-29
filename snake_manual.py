6c06bdc36ed5419ffe74a195571a5b60505a79e3
milind <milindchaudhary80@gmail.com>
2022-07-29 09:53:57 +0200

    important libraries added along with module description

---
+++ snake_manual.py
@@ -0,0 +1,10 @@
+""" This module consists of the same snake game
+but this is controlled manually """
+
+# importing important libraries
+import pygame
+import random
+
+from enum import Enum
+from collections import namedtuple
+
