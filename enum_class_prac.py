4c6731da8ac81662aea80a6177a8e22fc0b91991
milind <milindchaudhary80@gmail.com>
2022-07-29 10:36:26 +0200

    Days class built: Enum class from enum module is imported

---
+++ enum_class_prac.py
@@ -0,0 +1,10 @@
+""" Dummy module which contains Enum class for practice"""
+
+# from enum module imported Enum class 
+from enum import Enum
+
+# making a class Days inheriting from class ENUM from module
+class Days(Enum):
+    Sun =1
+    Mon = 2
+    Tue =3 
