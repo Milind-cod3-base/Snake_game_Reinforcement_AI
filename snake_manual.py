887823dba7c5b35f408371dcd1efec947b42e984
milind <milindchaudhary80@gmail.com>
2022-07-29 10:19:44 +0200

    class direction built: with four directions given a key

--- snake_manual.py
+++ snake_manual.py
@@ -8,3 +8,14 @@ import random
 from enum import Enum
 from collections import namedtuple
 
+pygame.init()
+# setting up the font
+font = pygame.font.Font('arial.ttf', 25)
+
+# making class of keys for the Direction 
+# inheriting from class Enum
+class Direction(Enum):
+    RIGHT = 1
+    LEFT = 2
+    UP =3
+    DOWN = 4
