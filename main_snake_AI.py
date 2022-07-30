8d7ad886a396769ff61e2f6e339558efc859bdc5
milind <milindchaudhary80@gmail.com>
2022-07-30 11:14:33 +0200

    pygame init with HUD font variable defined

--- main_snake_AI.py
+++ main_snake_AI.py
@@ -4,3 +4,9 @@ import random
 
 from enum import Enum
 from collections import namedtuple
+
+# initialize all imported pygame modules
+pygame.init()
+
+# setting up the font
+font = pygame.font.Font('arial.ttf', 25)
