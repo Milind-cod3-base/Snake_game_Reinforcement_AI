83a761a702d144a0468d79521b1e54eeb2fdeeb1
milind <milindchaudhary80@gmail.com>
2022-07-31 12:16:29 +0200

    important modules and classes imported

---
+++ snake_agent.py
@@ -0,0 +1,6 @@
+import torch
+import random
+import numpy as np
+from collections import deque 
+# importing classes and object from self made module
+from snake_AI_env import SnakeGameAI, Direction, Point
