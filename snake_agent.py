4db214a14a035f0c83364116908c4e3b3ca58234
milind <milindchaudhary80@gmail.com>
2022-07-31 13:13:19 +0200

    parameters set for agent

--- snake_agent.py
+++ snake_agent.py
@@ -4,3 +4,8 @@ import numpy as np
 from collections import deque 
 # importing classes and object from self made module
 from snake_AI_env import SnakeGameAI, Direction, Point
+
+# setting parameters
+MAX_MEMORY = 100_000
+BATCH_SIZE = 1000
+LR = 0.001
