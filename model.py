a383636666e3d563d1728afaf66cceec0f64b959
milind <milindchaudhary80@gmail.com>
2022-08-01 08:10:17 +0200

    Build class Linear_QNetwith constructor containing two NN layers

--- model.py
+++ model.py
@@ -9,3 +9,12 @@ import torch.nn as nn
 import torch.optim as optim
 import torch.nn.functional as F
 import os # to save the model
+
+
+# inheriting torch module and holding DQN
+class Linear_QNet(nn.Module):
+    def __init__(self, input_size, hidden_size, output_size ):
+        super().__init__() # init from parent class
+        # creating two layer neural network
+        self.linear1 = nn.Linear(input_size, hidden_size)
+        self.linear2 = nn.Linear(hidden_size, output_size) 
