2653e593a2681ce7de88760f05741ab3fe446e46
milind <milindchaudhary80@gmail.com>
2022-08-01 08:20:29 +0200

    forward propagation method: 2 layers, one with RELu activation

--- model.py
+++ model.py
@@ -4,6 +4,7 @@
 """
 
 # importing libraries
+from turtle import forward
 import torch
 import torch.nn as nn
 import torch.optim as optim
@@ -18,3 +19,12 @@ class Linear_QNet(nn.Module):
         # creating two layer neural network
         self.linear1 = nn.Linear(input_size, hidden_size)
         self.linear2 = nn.Linear(hidden_size, output_size) 
+
+    # creating a feed forward
+    def forward(self, x):
+        # using function module and relu as activation function
+        x = F.relu(self.linear1(x)) # linear layer with tensor x as input
+        # we dont need activation function as its last layer
+        x = self.linear2(x) # returning raw values
+        
+        return x
