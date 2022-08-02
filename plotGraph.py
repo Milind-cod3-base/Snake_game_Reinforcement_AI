1fc9b31524662a9d4b626b2a3a6a415e9bea6370
milind <milindchaudhary80@gmail.com>
2022-08-02 08:56:06 +0200

    Bug fix: plt.show and plt.pause missing

--- plotGraph.py
+++ plotGraph.py
@@ -1,5 +1,6 @@
 from IPython import display
 from matplotlib import pyplot as plt
+from numpy import block
 
 
 plt.ion()  # interactive mode on
@@ -16,4 +17,5 @@ def plot(scores, mean_scores):
     plt.ylim(ymin=0)
     plt.text(len(scores)-1, scores[-1], str(scores[-1]))
     plt.text(len(mean_scores)-1, mean_scores[-1], str(mean_scores[-1]))
-
+    plt.show(block= False)
+    plt.pause(.1)
