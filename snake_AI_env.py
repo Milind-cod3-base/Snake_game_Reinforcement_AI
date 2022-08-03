5551ab9067654b51d7685bc8f3b0905a72a593cb
milind <milindchaudhary80@gmail.com>
2022-08-03 15:44:18 +0200

    Increase length of snake to 5 to  solve the trailing problem

--- snake_AI_env.py
+++ snake_AI_env.py
@@ -87,7 +87,9 @@ class SnakeGameAI:
         # the snake. Starting with length of 3 blocks.
         self.snake = [self.head, 
                       Point(self.head.x-BLOCK_SIZE, self.head.y),
-                      Point(self.head.x-(2*BLOCK_SIZE), self.head.y)]
+                      Point(self.head.x-(2*BLOCK_SIZE), self.head.y),
+                      Point(self.head.x-(3*BLOCK_SIZE), self.head.y),
+                      Point(self.head.x-(4*BLOCK_SIZE), self.head.y)]
 
         # setting initial score to zero
         self.score = 0
