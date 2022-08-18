049fb1b9a8b0472678e325f73def91104a1a30eb
milind <milindchaudhary80@gmail.com>
2022-08-18 11:49:25 +0200

    Refactor: set pep8 style using autopep8

--- snake_AI_env.py
+++ snake_AI_env.py
@@ -19,43 +19,48 @@ font = pygame.font.Font('arial.ttf', 25)
 # play(action) -> direction
 # is_collision
 
-# making class of keys for the Direction 
+# making class of keys for the Direction
 # inheriting from class Enum
 # Enum is a class for creating enumerations which are a set of
 # symbolic names (members) bound to unique, constant values.
+
+
 class Direction(Enum):
     RIGHT = 1
     LEFT = 2
-    UP =3
+    UP = 3
     DOWN = 4
 
+
 #  creating a namedtuple for the x and y cordinates
-# basically it names the cordinates as x= and y= 
-Point = namedtuple('Point','x, y')
+# basically it names the cordinates as x= and y=
+Point = namedtuple('Point', 'x, y')
 
 # rgb colors
-WHITE = (255,255,255)
-BLUE1 = (0,0,255)
-GREEN = (0,255,0)
-BLACK = (0,0,0)
+WHITE = (255, 255, 255)
+BLUE1 = (0, 0, 255)
+GREEN = (0, 255, 0)
+BLACK = (0, 0, 0)
 
 # variable: size of food block
 BLOCK_SIZE = 20
 
 # variable: speed of the clock which refreshes the screen
 # indirectly speed of the snake
-SPEED = 60 
+SPEED = 60
 
-# creating a class for the AI handled snake game 
+# creating a class for the AI handled snake game
+
+
 class SnakeGameAI:
-    
+
     # a constructor with pre-defined window size of the game
     def __init__(self, w=640, h=480):
         self.w = w
-        self.h = h 
-        
+        self.h = h
+
         # init display
-        
+
         # set the window size
         self.display = pygame.display.set_mode((self.w, self.h))
 
@@ -69,11 +74,10 @@ class SnakeGameAI:
         # calling the reset method here in the constructor
         self.reset()
 
-        
-
     # setting up a reset method for the ai
     # copied code from init constructor
     # which init the game state and pasted
+
     def reset(self):
         # init a game state
         # using RIGHT from the enum inherited class Direction
@@ -81,53 +85,53 @@ class SnakeGameAI:
 
         # putting the head of the snake while init
         # in the middle of the screen
-        self.head = Point(self.w/2, self.h/2)
+        self.head = Point(self.w / 2, self.h / 2)
 
-        # creating a list which contains the attributes of 
+        # creating a list which contains the attributes of
         # the snake. Starting with length of 3 blocks.
-        self.snake = [self.head, 
-                      Point(self.head.x-BLOCK_SIZE, self.head.y),
-                      Point(self.head.x-(2*BLOCK_SIZE), self.head.y),
-                      Point(self.head.x-(3*BLOCK_SIZE), self.head.y),
-                      Point(self.head.x-(4*BLOCK_SIZE), self.head.y)]
+        self.snake = [self.head,
+                      Point(self.head.x - BLOCK_SIZE, self.head.y),
+                      Point(self.head.x - (2 * BLOCK_SIZE), self.head.y),
+                      Point(self.head.x - (3 * BLOCK_SIZE), self.head.y),
+                      Point(self.head.x - (4 * BLOCK_SIZE), self.head.y)]
 
         # setting initial score to zero
         self.score = 0
         # food as none in init
-        self.food = None 
+        self.food = None
 
         # placing the food in the game screen using method
         # using a leading underscore for making it for internal use
         # only
         self._place_food()
 
-        # setting the frame iteration counter to 0 
+        # setting the frame iteration counter to 0
         self.frame_iteration = 0
 
-    
     # creating a method for placing the food randomly in game display
     # this method is for internal use only
+
     def _place_food(self):
-        
+
         # setting cordinates which must inside the boundary of display
-        x = random.randint(0, (self.w-BLOCK_SIZE)//BLOCK_SIZE)*BLOCK_SIZE
-        y = random.randint(0, (self.h-BLOCK_SIZE)//BLOCK_SIZE)*BLOCK_SIZE
+        x = random.randint(0, (self.w - BLOCK_SIZE) // BLOCK_SIZE) * BLOCK_SIZE
+        y = random.randint(0, (self.h - BLOCK_SIZE) // BLOCK_SIZE) * BLOCK_SIZE
 
         # assigning attribute for food position
-        self.food = Point(x,y)
-        
+        self.food = Point(x, y)
+
         # if cordinates of food and cordinates of snakes coincides
         # and it means the snake has eaten the food, new food has to
-        # be placed again. 
+        # be placed again.
         # Using recursive style on this method to replace the food
         if self.food in self.snake:
-            self._place_food()  
+            self._place_food()
 
-   
     # creating a method for the snake to take steps
     # this function now takes action parameter which is given by AI
+
     def play_step(self, action):
-        self.frame_iteration +=1
+        self.frame_iteration += 1
 
         # step1: collecting user input
 
@@ -141,23 +145,20 @@ class SnakeGameAI:
             if event.type == pygame.QUIT:
                 # quits the pygame
                 pygame.quit()
-                #quits the code
-                quit()  
+                # quits the code
+                quit()
 
-        
         # step2: moving action
-        
+
         # this updates the head of the snake
         # also resets the value of self.head attribute
         # it takes input action given by the AI
         self._move(action)
-         
-
 
         # inserts self.head before the index 0 in the snake list
         # takes the new self.head value and puts it inside the snake
         # and makes its new head
-        self.snake.insert(0, self.head) 
+        self.snake.insert(0, self.head)
 
         # step3: check if game over
 
@@ -166,72 +167,70 @@ class SnakeGameAI:
         # setting game_over variable as False
         game_over = False
 
-        #  if the collision occurs, game must be over 
+        #  if the collision occurs, game must be over
         # results must be displayed
         # or if the frame keeps on going and snake length doesnt change
         # its jus moving without eating then stop using a custom formula
-        if self.is_collision() or self.frame_iteration > 100*len(self.snake):
+        if self.is_collision() or self.frame_iteration > 100 * len(self.snake):
             game_over = True
             reward = - 10   # collision or longer gameplay without increase
             return reward, game_over, self.score
 
-
-
         # step4: place new food if snake eats or just move
         if self.head == self.food:
             # increase score by one
-            self.score +=1
+            self.score += 1
             # give reward
             reward = 10
             # after eating place the food again
             self._place_food()
-        
+
         # if snake doesnt eat anything, pop the last part of the snake
         # this will keep the length of the snake same
         else:
             self.snake.pop()
-        
+
         # step5:  update ui and clock
         self._update_ui()
         self.clock.tick(SPEED)
 
         # step6: return game over and score
         return game_over, self.score, reward
-    
+
     # method for if collision happens -> returns boolean value
     # parameter pt is set to None, no need to put parameter when calling it
-    # pt is introduced to detect the danger or states 
+    # pt is introduced to detect the danger or states
     """ 11 states are: [ danger straight, danger right, danger left,
-                            direction left, direction right, 
+                            direction left, direction right,
                             direction up, direction down,
                             food left, food right,
                             food up, food down]"""
-    
-    #earlier this method had leading underscore: internal. 
+
+    # earlier this method had leading underscore: internal.
     # But this was a mistake, as the agent module wants to use this
     # Hence leading underscore is removed, making it a public method
-    def is_collision(self, pt= None):
+    def is_collision(self, pt=None):
 
-        if pt is None: # checking if the parameter is set to None
+        if pt is None:  # checking if the parameter is set to None
             # then assign the value self.head to the parameter
             pt = self.head
 
         # hits boundary
         if pt.x > self.w - BLOCK_SIZE or pt.x < 0 or pt.y > self.h - BLOCK_SIZE or pt.y < 0:
             return True
-        
+
         # hits itself
-        if pt in self.snake[1:]:  
+        if pt in self.snake[1:]:
             # if the head is in the snake and its 3rd or later element
             # then it has hit itself
 
             return True
 
         return False
-    
+
     # update the UI
     def _update_ui(self):
-        
+
         # creating the background black
         self.display.fill(BLACK)
 
@@ -242,12 +241,23 @@ class SnakeGameAI:
         for pt in self.snake:  # iterating using pt below
 
             # laying base color for foundation
-            pygame.draw.rect(self.display, BLUE1, pygame.Rect(pt.x, pt.y, BLOCK_SIZE, BLOCK_SIZE ))
+            pygame.draw.rect(
+                self.display, BLUE1, pygame.Rect(
+                    pt.x, pt.y, BLOCK_SIZE, BLOCK_SIZE))
             # laying another layer of color on top of base color
-            pygame.draw.rect(self.display, GREEN, pygame.Rect(pt.x+4, pt.y+4, 12, 12 ))
+            pygame.draw.rect(
+                self.display, GREEN, pygame.Rect(
+                    pt.x + 4, pt.y + 4, 12, 12))
 
-        # this is to refresh the food block    
-        pygame.draw.rect(self.display, WHITE, pygame.Rect(self.food.x, self.food.y, BLOCK_SIZE, BLOCK_SIZE))
+        # this is to refresh the food block
+        pygame.draw.rect(
+            self.display,
+            WHITE,
+            pygame.Rect(
+                self.food.x,
+                self.food.y,
+                BLOCK_SIZE,
+                BLOCK_SIZE))
 
         # refreshing the UI
 
@@ -255,62 +265,69 @@ class SnakeGameAI:
         text = font.render("Score: " + str(self.score), True, WHITE)
 
         # set the position for HUD
-        self.display.blit(text, [0,0])
+        self.display.blit(text, [0, 0])
 
         # updating the entire screen by using flip
         #: if only portion of screen has to be updated, use update method instead of flip
         pygame.display.flip()
 
-        
-   
-    
     """
     Action (all are relative direction depending on current action)
 
-    [1,0,0] -> straight 
+    [1,0,0] -> straight
     [0,1,0] -> right turn
     [0,0,1] -> left turn
     """
-     # method to move the snake further
+    # method to move the snake further
+
     def _move(self, action):
         # [straight, right, left]
 
         # directions listed in clockwise format
-        clock_wise = [Direction.RIGHT, Direction.DOWN, Direction.LEFT, Direction.UP]
-       
+        clock_wise = [
+            Direction.RIGHT,
+            Direction.DOWN,
+            Direction.LEFT,
+            Direction.UP]
+
         # gets the index of the current direction frm clock_wise list
         idx = clock_wise.index(self.direction)
 
-        # np.array_equal(a1,a2) is true if two arrays are same shape and elements
-    
-        if np.array_equal(action, [1, 0, 0] ): # we keep the current direction
-            new_dir = clock_wise[idx]  
-        
-        elif np.array_equal(action, [0, 1, 0] ): # all relative
+        # np.array_equal(a1,a2) is true if two arrays are same shape and
+        # elements
+
+        if np.array_equal(action, [1, 0, 0]):  # we keep the current direction
+            new_dir = clock_wise[idx]
+
+        elif np.array_equal(action, [0, 1, 0]):  # all relative
             # if the ai says to turn right, we have to make it right relative to current direction
-            # hence we need to take next index in the clockwise direction list. We dont want absolute right.
+            # hence we need to take next index in the clockwise direction list.
+            # We dont want absolute right.
 
             # rotating index
             next_idx = (idx + 1) % 4  # this gives me the remainder
-            
+
             # new direction
-            new_dir = clock_wise[next_idx]  # right turn r -> d -> l -> u (turns right, if already right, then down etc.)
-        
-        else: #[0,0,1]
-            # if ai says to turn left, we take relative left, relative to current direction
-            next_idx = (idx-1) % 4 # moving backwards in the list
-            
-            new_dir = clock_wise[next_idx] # left turn r -> u -> l -> d
+            # right turn r -> d -> l -> u (turns right, if already right, then
+            # down etc.)
+            new_dir = clock_wise[next_idx]
+
+        else:  # [0,0,1]
+            # if ai says to turn left, we take relative left, relative to
+            # current direction
+            next_idx = (idx - 1) % 4  # moving backwards in the list
+
+            new_dir = clock_wise[next_idx]  # left turn r -> u -> l -> d
 
         self.direction = new_dir
-       
-        x = self.head.x # calling attribute head of snake's x cordinate
+
+        x = self.head.x  # calling attribute head of snake's x cordinate
         y = self.head.y
 
         if self.direction == Direction.RIGHT:
             # updating x
             x += BLOCK_SIZE
-        
+
         elif self.direction == Direction.LEFT:
             # updating x
             x -= BLOCK_SIZE
@@ -324,7 +341,7 @@ class SnakeGameAI:
             y -= BLOCK_SIZE
 
         # reseting self.head values to the new cordinates
-        self.head = Point(x,y)
+        self.head = Point(x, y)
 
 
 """
