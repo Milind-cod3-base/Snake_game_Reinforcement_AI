23985ad4c8ce76128fb74db2785ff094e2e7df36
milind <milindchaudhary80@gmail.com>
2022-10-19 16:44:48 +0200

    Set color green

--- snake_manual.py
+++ snake_manual.py
@@ -31,9 +31,8 @@ Point = namedtuple('Point','x, y')
 
 # rgb colors
 WHITE = (255,255,255)
-RED = (200,0,0)
 BLUE1 = (0,0,255)
-BLUE2 = (0,100,255)
+GREEN = (0,255,0)
 BLACK = (0,0,0)
 
 # variable: size of food block
